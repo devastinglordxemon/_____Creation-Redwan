48289fff46ccd2146d73eb6cd1e298b057ecd293
devastinglordxemon <159053101+devastinglordxemon@users.noreply.github.com>
2024-06-09 16:46:46 +0600

    Update Høpéléss.js

--- scripts/cmds/Høpéléss.js
+++ scripts/cmds/Høpéléss.js
@@ -1,8 +1,8 @@
 module.exports = {
     config: {
-        name: "høpéléss mâhî",
+        name: "redwan",
         version: "1.0",
-        author: "MAHI", //** original author fb I'd : https://m.me/MR.AYAN.2X **//
+        author: "MAHI", 
         countDown: 5,
         role: 0,
         shortDescription: "No Prefix",
